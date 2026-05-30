6d172eba5b9129625646092356ef32b90a6b7d46
julionvc90 <ju.navarroc@duocuc.cl>
2026-05-30 17:32:43 -0400

    Base.

---
+++ inspeccion/src/main/java/com/example/inspeccion/InspeccionApplication.java
@@ -0,0 +1,13 @@
+package com.example.inspeccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspeccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspeccionApplication.class, args);
+	}
+
+}
---
+++ inspeccion/src/test/java/com/example/inspeccion/InspeccionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inspeccion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspeccionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
